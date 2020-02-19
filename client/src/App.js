1abf2352191e67c5b7b3de1dcdf25f8635386bb3
github username <alekvuka@gmail.com>
2020-02-18 23:53:46 -0500

    add src to CamerasContainer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import HeaderContainer from './src/Containers/HeaderContainer.js'
-import CamerasContainer from './Containers/CamerasContainer.js'
+import CamerasContainer from './src/Containers/CamerasContainer.js'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import RequestContainer from './Containers/RequestContainer.js'
 import HomePage from './Components/HomePage.js'
