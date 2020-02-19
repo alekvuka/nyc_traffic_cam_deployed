4d2b381a4cab47292e2e5b19edd030645ce28c11
github username <alekvuka@gmail.com>
2020-02-18 23:59:05 -0500

    add src to home container path

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import HeaderContainer from './src/Containers/HeaderContainer.js'
 import CamerasContainer from './src/Containers/CamerasContainer.js'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import RequestContainer from './src/Containers/RequestContainer.js'
-import HomePage from './Components/HomePage.js'
+import HomePage from './src./Components/HomePage.js'
 
 class App extends Component {
 
